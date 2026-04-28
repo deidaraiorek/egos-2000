e88f575f260cff1ff599d072faef064ee957839c
Dang Pham <dangpham@Dangs-MacBook-Air.local>
2026-04-27 21:37:42 -0400

    Collect process lifecycle statistics

--- grass/process.h
+++ grass/process.h
@@ -4,24 +4,30 @@
 #include "syscall.h"
 
 enum proc_status {
-    PROC_UNUSED,
-    PROC_LOADING,
-    PROC_READY,
-    PROC_RUNNING,
-    PROC_RUNNABLE,
-    PROC_PENDING_SYSCALL
+  PROC_UNUSED,
+  PROC_LOADING,
+  PROC_READY,
+  PROC_RUNNING,
+  PROC_RUNNABLE,
+  PROC_PENDING_SYSCALL
 };
 
 struct process {
-    int pid;
-    struct syscall syscall;
-    enum proc_status status;
-    uint mepc, saved_registers[32];
-    /* Student's code goes here (Preemptive Scheduler | System Call). */
+  int pid;
+  struct syscall syscall;
+  enum proc_status status;
+  uint mepc, saved_registers[32];
+  /* Student's code goes here (Preemptive Scheduler | System Call). */
+  ulonglong creation_timestamp;
+  ulonglong first_scheduled_timestamp;
+  ulonglong response_time;
+  ulonglong turn_around_time;
+  ulonglong run_start_timestamp;
+  ulonglong cpu_run_time;
+  int timer_interrupt_count;
+  /* Add new fields for lifecycle statistics, MLFQ or process sleep. */
 
-    /* Add new fields for lifecycle statistics, MLFQ or process sleep. */
-
-    /* Student's code ends here. */
+  /* Student's code ends here. */
 };
 #define MAX_NPROCESS 16
 
@@ -35,7 +41,7 @@ void proc_set_runnable(int);
 void proc_set_pending(int);
 
 void mlfq_reset_level();
-void mlfq_update_level(struct process* p, ulonglong runtime);
+void mlfq_update_level(struct process *p, ulonglong runtime);
 void proc_sleep(int pid, uint usec);
 void proc_coresinfo();
 
